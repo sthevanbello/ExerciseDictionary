15f0a9654daff225d8a1a109db7dd2be67003be9
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-17 21:21:45 -0300

    Exercise dictionary reading file .csv

---
+++ ExerciseDictionary/Entities/Candidate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDictionary.Entities
+{
+    class Candidate
+    {
+        public string Name { get; set; }
+
+        public int Vote { get; set; }
+
+        public Candidate(string name, int vote)
+        {
+            Name = name;
+            Vote = vote;
+           
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Candidate))
+            {
+                return false;
+            }
+            Candidate other = obj as Candidate;
+
+            return Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        
+    }
+}
---
+++ ExerciseDictionary/Program.cs
@@ -0,0 +1,60 @@
+﻿using ExerciseDictionary.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExerciseDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp\Dictionary\in.csv";
+
+            Dictionary<string, int> candidatesAll = new Dictionary<string, int>();
+
+            Candidate candidates;
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        candidates = new Candidate(name, votes);
+
+                        if (!candidatesAll.ContainsKey(name))
+                        {
+                            candidatesAll[candidates.Name] = candidates.Vote;
+                        }
+                        else
+                        {
+                            candidatesAll[candidates.Name] += candidates.Vote;
+                        }
+
+                    }
+
+                }
+
+                foreach (KeyValuePair<string, int> item in candidatesAll)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
